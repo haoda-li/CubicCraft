3edf045bb49472a3829ca1f3ef74a5c43a1d30b2
weijili <weijili@umich.edu>
2023-04-24 10:54:16 -0700

    add step size to GUI

--- GUI/example_GUI.py
+++ GUI/example_GUI.py
@@ -11,7 +11,7 @@ import time
 get to the main
 '''
 sys.path.append('../main')
-from cube_stylier_cpu import *
+from cube_stylizer_cpu import *
 
 isMacOS = (platform.system() == "Darwin")
 
@@ -147,6 +147,7 @@ class Settings:
         self.sun_intensity = 45000
         self.sun_dir = [0.577, -0.577, -0.577]
         self.sun_color = gui.Color(1, 1, 1)
+        self.step_size = 10
 
         self.apply_material = True  # clear to False after processing
         self._materials = {
@@ -248,173 +249,168 @@ class AppWindow:
         # space for all its children when open, but only enough for text when
         # closed. This is useful for property pages, so the user can hide sets
         # of properties they rarely use.
-        view_ctrls = gui.CollapsableVert("View controls", 0.25 * em,
-                                         gui.Margins(em, 0, 0, 0))
+        # view_ctrls = gui.CollapsableVert("View controls", 0.25 * em,
+        #                                  gui.Margins(em, 0, 0, 0))
 
-        self._arcball_button = gui.Button("Arcball")
-        self._arcball_button.horizontal_padding_em = 0.5
-        self._arcball_button.vertical_padding_em = 0
-        self._arcball_button.set_on_clicked(self._set_mouse_mode_rotate)
-        self._fly_button = gui.Button("Fly")
-        self._fly_button.horizontal_padding_em = 0.5
-        self._fly_button.vertical_padding_em = 0
-        self._fly_button.set_on_clicked(self._set_mouse_mode_fly)
-        self._model_button = gui.Button("Model")
-        self._model_button.horizontal_padding_em = 0.5
-        self._model_button.vertical_padding_em = 0
-        self._model_button.set_on_clicked(self._set_mouse_mode_model)
-        self._sun_button = gui.Button("Sun")
-        self._sun_button.horizontal_padding_em = 0.5
-        self._sun_button.vertical_padding_em = 0
-        self._sun_button.set_on_clicked(self._set_mouse_mode_sun)
-        self._ibl_button = gui.Button("Environment")
-        self._ibl_button.horizontal_padding_em = 0.5
-        self._ibl_button.vertical_padding_em = 0
-        self._ibl_button.set_on_clicked(self._set_mouse_mode_ibl)
-        view_ctrls.add_child(gui.Label("Mouse controls"))
+        # self._arcball_button = gui.Button("Arcball")
+        # self._arcball_button.horizontal_padding_em = 0.5
+        # self._arcball_button.vertical_padding_em = 0
+        # self._arcball_button.set_on_clicked(self._set_mouse_mode_rotate)
+        # self._fly_button = gui.Button("Fly")
+        # self._fly_button.horizontal_padding_em = 0.5
+        # self._fly_button.vertical_padding_em = 0
+        # self._fly_button.set_on_clicked(self._set_mouse_mode_fly)
+        # self._model_button = gui.Button("Model")
+        # self._model_button.horizontal_padding_em = 0.5
+        # self._model_button.vertical_padding_em = 0
+        # self._model_button.set_on_clicked(self._set_mouse_mode_model)
+        # self._sun_button = gui.Button("Sun")
+        # self._sun_button.horizontal_padding_em = 0.5
+        # self._sun_button.vertical_padding_em = 0
+        # self._sun_button.set_on_clicked(self._set_mouse_mode_sun)
+        # self._ibl_button = gui.Button("Environment")
+        # self._ibl_button.horizontal_padding_em = 0.5
+        # self._ibl_button.vertical_padding_em = 0
+        # self._ibl_button.set_on_clicked(self._set_mouse_mode_ibl)
+        # view_ctrls.add_child(gui.Label("Mouse controls"))
         # We want two rows of buttons, so make two horizontal layouts. We also
         # want the buttons centered, which we can do be putting a stretch item
         # as the first and last item. Stretch items take up as much space as
         # possible, and since there are two, they will each take half the extra
         # space, thus centering the buttons.
-        h = gui.Horiz(0.25 * em)  # row 1
-        h.add_stretch()
-        h.add_child(self._arcball_button)
-        h.add_child(self._fly_button)
-        h.add_child(self._model_button)
-        h.add_stretch()
-        view_ctrls.add_child(h)
-        h = gui.Horiz(0.25 * em)  # row 2
-        h.add_stretch()
-        h.add_child(self._sun_button)
-        h.add_child(self._ibl_button)
-        h.add_stretch()
-        view_ctrls.add_child(h)
+        # h = gui.Horiz(0.25 * em)  # row 1
+        # h.add_stretch()
+        # h.add_child(self._arcball_button)
+        # h.add_child(self._fly_button)
+        # h.add_child(self._model_button)
+        # h.add_stretch()
+        # view_ctrls.add_child(h)
+        # h = gui.Horiz(0.25 * em)  # row 2
+        # h.add_stretch()
+        # h.add_child(self._sun_button)
+        # h.add_child(self._ibl_button)
+        # h.add_stretch()
+        # view_ctrls.add_child(h)
 
-        self._show_skybox = gui.Checkbox("Show skymap")
-        self._show_skybox.set_on_checked(self._on_show_skybox)
-        view_ctrls.add_fixed(separation_height)
-        view_ctrls.add_child(self._show_skybox)
+        # self._show_skybox = gui.Checkbox("Show skymap")
+        # self._show_skybox.set_on_checked(self._on_show_skybox)
+        # view_ctrls.add_fixed(separation_height)
+        # view_ctrls.add_child(self._show_skybox)
 
-        self._bg_color = gui.ColorEdit()
-        self._bg_color.set_on_value_changed(self._on_bg_color)
+        # self._bg_color = gui.ColorEdit()
+        # self._bg_color.set_on_value_changed(self._on_bg_color)
 
-        grid = gui.VGrid(2, 0.25 * em)
-        grid.add_child(gui.Label("BG Color"))
-        grid.add_child(self._bg_color)
-        view_ctrls.add_child(grid)
+        # grid = gui.VGrid(2, 0.25 * em)
+        # grid.add_child(gui.Label("BG Color"))
+        # grid.add_child(self._bg_color)
+        # view_ctrls.add_child(grid)
 
-        self._show_axes = gui.Checkbox("Show axes")
-        self._show_axes.set_on_checked(self._on_show_axes)
-        view_ctrls.add_fixed(separation_height)
-        view_ctrls.add_child(self._show_axes)
+        # self._show_axes = gui.Checkbox("Show axes")
+        # self._show_axes.set_on_checked(self._on_show_axes)
+        # view_ctrls.add_fixed(separation_height)
+        # view_ctrls.add_child(self._show_axes)
 
-        self._profiles = gui.Combobox()
-        for name in sorted(Settings.LIGHTING_PROFILES.keys()):
-            self._profiles.add_item(name)
-        self._profiles.add_item(Settings.CUSTOM_PROFILE_NAME)
-        self._profiles.set_on_selection_changed(self._on_lighting_profile)
-        view_ctrls.add_fixed(separation_height)
-        view_ctrls.add_child(gui.Label("Lighting profiles"))
-        view_ctrls.add_child(self._profiles)
-        self._settings_panel.add_fixed(separation_height)
-        self._settings_panel.add_child(view_ctrls)
+        # self._profiles = gui.Combobox()
+        # for name in sorted(Settings.LIGHTING_PROFILES.keys()):
+        #     self._profiles.add_item(name)
+        # self._profiles.add_item(Settings.CUSTOM_PROFILE_NAME)
+        # self._profiles.set_on_selection_changed(self._on_lighting_profile)
+        # view_ctrls.add_fixed(separation_height)
+        # view_ctrls.add_child(gui.Label("Lighting profiles"))
+        # view_ctrls.add_child(self._profiles)
+        # self._settings_panel.add_fixed(separation_height)
+        # self._settings_panel.add_child(view_ctrls)
 
-        # How to get those settings?
-        # todo: add on_change function
-        cubic = gui.CollapsableVert("Cubic", 0, gui.Margins(em, 0, 0, 0))
-        cubic.set_is_open(True)
-        cubic.add_child(gui.Label("Test"))
-        cubic.add_child(gui.Slider(gui.Slider.INT))
-        self._settings_panel.add_child(cubic)
-
-        advanced = gui.CollapsableVert("Advanced lighting", 0,
-                                       gui.Margins(em, 0, 0, 0))
-        advanced.set_is_open(False)
+        # advanced = gui.CollapsableVert("Advanced lighting", 0,
+        #                                gui.Margins(em, 0, 0, 0))
+        # advanced.set_is_open(False)
         
-        self._use_ibl = gui.Checkbox("HDR map")
-        self._use_ibl.set_on_checked(self._on_use_ibl)
-        self._use_sun = gui.Checkbox("Sun")
-        self._use_sun.set_on_checked(self._on_use_sun)
-        advanced.add_child(gui.Label("Light sources"))
-        h = gui.Horiz(em)
-        h.add_child(self._use_ibl)
-        h.add_child(self._use_sun)
-        advanced.add_child(h)
+        # self._use_ibl = gui.Checkbox("HDR map")
+        # self._use_ibl.set_on_checked(self._on_use_ibl)
+        # self._use_sun = gui.Checkbox("Sun")
+        # self._use_sun.set_on_checked(self._on_use_sun)
+        # advanced.add_child(gui.Label("Light sources"))
+        # h = gui.Horiz(em)
+        # h.add_child(self._use_ibl)
+        # h.add_child(self._use_sun)
+        # advanced.add_child(h)
 
-        self._ibl_map = gui.Combobox()
-        for ibl in glob.glob(gui.Application.instance.resource_path +
-                             "/*_ibl.ktx"):
+        # self._ibl_map = gui.Combobox()
+        # for ibl in glob.glob(gui.Application.instance.resource_path +
+        #                      "/*_ibl.ktx"):
 
-            self._ibl_map.add_item(os.path.basename(ibl[:-8]))
-        self._ibl_map.selected_text = AppWindow.DEFAULT_IBL
-        self._ibl_map.set_on_selection_changed(self._on_new_ibl)
-        self._ibl_intensity = gui.Slider(gui.Slider.INT)
-        self._ibl_intensity.set_limits(0, 200000)
-        self._ibl_intensity.set_on_value_changed(self._on_ibl_intensity)
-        grid = gui.VGrid(2, 0.25 * em)
-        grid.add_child(gui.Label("HDR map"))
-        grid.add_child(self._ibl_map)
-        grid.add_child(gui.Label("Intensity"))
-        grid.add_child(self._ibl_intensity)
-        advanced.add_fixed(separation_height)
-        advanced.add_child(gui.Label("Environment"))
-        advanced.add_child(grid)
+        #     self._ibl_map.add_item(os.path.basename(ibl[:-8]))
+        # self._ibl_map.selected_text = AppWindow.DEFAULT_IBL
+        # self._ibl_map.set_on_selection_changed(self._on_new_ibl)
+        # self._ibl_intensity = gui.Slider(gui.Slider.INT)
+        # self._ibl_intensity.set_limits(0, 200000)
+        # self._ibl_intensity.set_on_value_changed(self._on_ibl_intensity)
+        # grid = gui.VGrid(2, 0.25 * em)
+        # grid.add_child(gui.Label("HDR map"))
+        # grid.add_child(self._ibl_map)
+        # grid.add_child(gui.Label("Intensity"))
+        # grid.add_child(self._ibl_intensity)
+        # advanced.add_fixed(separation_height)
+        # advanced.add_child(gui.Label("Environment"))
+        # advanced.add_child(grid)
 
-        self._sun_intensity = gui.Slider(gui.Slider.INT)
-        self._sun_intensity.set_limits(0, 200000)
-        self._sun_intensity.set_on_value_changed(self._on_sun_intensity)
-        self._sun_dir = gui.VectorEdit()
-        self._sun_dir.set_on_value_changed(self._on_sun_dir)
-        self._sun_color = gui.ColorEdit()
-        self._sun_color.set_on_value_changed(self._on_sun_color)
-        grid = gui.VGrid(2, 0.25 * em)
-        grid.add_child(gui.Label("Intensity"))
-        grid.add_child(self._sun_intensity)
-        grid.add_child(gui.Label("Direction"))
-        grid.add_child(self._sun_dir)
-        grid.add_child(gui.Label("Color"))
-        grid.add_child(self._sun_color)
-        advanced.add_fixed(separation_height)
-        advanced.add_child(gui.Label("Sun (Directional light)"))
-        advanced.add_child(grid)
+        # self._sun_intensity = gui.Slider(gui.Slider.INT)
+        # self._sun_intensity.set_limits(0, 200000)
+        # self._sun_intensity.set_on_value_changed(self._on_sun_intensity)
+        # self._sun_dir = gui.VectorEdit()
+        # self._sun_dir.set_on_value_changed(self._on_sun_dir)
+        # self._sun_color = gui.ColorEdit()
+        # self._sun_color.set_on_value_changed(self._on_sun_color)
+        # grid = gui.VGrid(2, 0.25 * em)
+        # grid.add_child(gui.Label("Intensity"))
+        # grid.add_child(self._sun_intensity)
+        # grid.add_child(gui.Label("Direction"))
+        # grid.add_child(self._sun_dir)
+        # grid.add_child(gui.Label("Color"))
+        # grid.add_child(self._sun_color)
+        # advanced.add_fixed(separation_height)
+        # advanced.add_child(gui.Label("Sun (Directional light)"))
+        # advanced.add_child(grid)
 
-        self._settings_panel.add_fixed(separation_height)
-        self._settings_panel.add_child(advanced)
+        # self._settings_panel.add_fixed(separation_height)
+        # self._settings_panel.add_child(advanced)
 
-        material_settings = gui.CollapsableVert("Material settings", 0,
+        # material_settings = gui.CollapsableVert("Material settings", 0,
+        #                                         gui.Margins(em, 0, 0, 0))
+
+        # self._shader = gui.Combobox()
+        # self._shader.add_item(AppWindow.MATERIAL_NAMES[0])
+        # self._shader.add_item(AppWindow.MATERIAL_NAMES[1])
+        # self._shader.add_item(AppWindow.MATERIAL_NAMES[2])
+        # self._shader.add_item(AppWindow.MATERIAL_NAMES[3])
+        # self._shader.set_on_selection_changed(self._on_shader)
+        # self._material_prefab = gui.Combobox()
+        # for prefab_name in sorted(Settings.PREFAB.keys()):
+        #     self._material_prefab.add_item(prefab_name)
+        # self._material_prefab.selected_text = Settings.DEFAULT_MATERIAL_NAME
+        # self._material_prefab.set_on_selection_changed(self._on_material_prefab)
+        # self._material_color = gui.ColorEdit()
+        # self._material_color.set_on_value_changed(self._on_material_color)
+        # self._point_size = gui.Slider(gui.Slider.INT)
+        # self._point_size.set_limits(1, 10)
+        # self._point_size.set_on_value_changed(self._on_point_size)
+
+        """
+        Own settings
+        """
+        cubic_settings = gui.CollapsableVert("Cubic settings", 0,
                                                 gui.Margins(em, 0, 0, 0))
-
-        self._shader = gui.Combobox()
-        self._shader.add_item(AppWindow.MATERIAL_NAMES[0])
-        self._shader.add_item(AppWindow.MATERIAL_NAMES[1])
-        self._shader.add_item(AppWindow.MATERIAL_NAMES[2])
-        self._shader.add_item(AppWindow.MATERIAL_NAMES[3])
-        self._shader.set_on_selection_changed(self._on_shader)
-        self._material_prefab = gui.Combobox()
-        for prefab_name in sorted(Settings.PREFAB.keys()):
-            self._material_prefab.add_item(prefab_name)
-        self._material_prefab.selected_text = Settings.DEFAULT_MATERIAL_NAME
-        self._material_prefab.set_on_selection_changed(self._on_material_prefab)
-        self._material_color = gui.ColorEdit()
-        self._material_color.set_on_value_changed(self._on_material_color)
-        self._point_size = gui.Slider(gui.Slider.INT)
-        self._point_size.set_limits(1, 10)
-        self._point_size.set_on_value_changed(self._on_point_size)
+        
+        self._step_size = gui.Slider(gui.Slider.INT)
+        self._step_size.set_limits(1, 10)
+        self._step_size.set_on_value_changed(self._on_step_size)
 
         grid = gui.VGrid(2, 0.25 * em)
-        grid.add_child(gui.Label("Type"))
-        grid.add_child(self._shader)
-        grid.add_child(gui.Label("Material"))
-        grid.add_child(self._material_prefab)
-        grid.add_child(gui.Label("Color"))
-        grid.add_child(self._material_color)
-        grid.add_child(gui.Label("Point size"))
-        grid.add_child(self._point_size)
-        material_settings.add_child(grid)
+        grid.add_child(gui.Label("Step size"))
+        grid.add_child(self._step_size)
+        cubic_settings.add_child(grid)
 
-        self._settings_panel.add_fixed(separation_height)
-        self._settings_panel.add_child(material_settings)
+        self._settings_panel.add_child(cubic_settings)
         # ----
 
         # Normally our user interface can be children of all one layout (usually
@@ -523,23 +519,24 @@ class AppWindow:
             self._scene.scene.update_material(self.settings.material)
             self.settings.apply_material = False
 
-        self._bg_color.color_value = self.settings.bg_color
-        self._show_skybox.checked = self.settings.show_skybox
-        self._show_axes.checked = self.settings.show_axes
-        self._use_ibl.checked = self.settings.use_ibl
-        self._use_sun.checked = self.settings.use_sun
-        self._ibl_intensity.int_value = self.settings.ibl_intensity
-        self._sun_intensity.int_value = self.settings.sun_intensity
-        self._sun_dir.vector_value = self.settings.sun_dir
-        self._sun_color.color_value = self.settings.sun_color
-        self._material_prefab.enabled = (
-            self.settings.material.shader == Settings.LIT)
-        c = gui.Color(self.settings.material.base_color[0],
-                      self.settings.material.base_color[1],
-                      self.settings.material.base_color[2],
-                      self.settings.material.base_color[3])
-        self._material_color.color_value = c
-        self._point_size.double_value = self.settings.material.point_size
+        # self._bg_color.color_value = self.settings.bg_color
+        # self._show_skybox.checked = self.settings.show_skybox
+        # self._show_axes.checked = self.settings.show_axes
+        # self._use_ibl.checked = self.settings.use_ibl
+        # self._use_sun.checked = self.settings.use_sun
+        # self._ibl_intensity.int_value = self.settings.ibl_intensity
+        # self._sun_intensity.int_value = self.settings.sun_intensity
+        # self._sun_dir.vector_value = self.settings.sun_dir
+        # self._sun_color.color_value = self.settings.sun_color
+        # self._material_prefab.enabled = (
+        #     self.settings.material.shader == Settings.LIT)
+        # c = gui.Color(self.settings.material.base_color[0],
+        #               self.settings.material.base_color[1],
+        #               self.settings.material.base_color[2],
+        #               self.settings.material.base_color[3])
+        # self._material_color.color_value = c
+        # self._point_size.double_value = self.settings.material.point_size
+        self._step_size.double_value = self.settings.step_size
 
     def _on_layout(self, layout_context):
         # The on_layout callback should set the frame (position + size) of every
@@ -640,6 +637,10 @@ class AppWindow:
     def _on_point_size(self, size):
         self.settings.material.point_size = int(size)
         self.settings.apply_material = True
+        self._apply_settings()
+        
+    def _on_step_size(self, size):
+        self.settings.step_size = int(size)
         self._apply_settings()
 
     def _on_menu_open(self):
@@ -766,7 +767,7 @@ class AppWindow:
             self.window.show_dialog(dlg)
         else:
             dlg_layout.add_child(gui.Label("Start to do the cubic stylization!"))
-            for i in range(10):
+            for i in range(self.settings.step_size):
                 print(f"\033[34m[INFO] Interation step: {i}\033[0m")
                 self.load_cubic_style()
                 self._scene.force_redraw()
@@ -809,7 +810,7 @@ class AppWindow:
                     """
                     add self.cube
                     """
-                    self.cube = CubeStylier(path)
+                    self.cube = CubeStylizer(path)
                 else:
                     # Point cloud
                     self._scene.scene.add_geometry("__model__", geometry,
